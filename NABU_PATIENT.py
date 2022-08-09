8beacdf4742c5b07e512e325119f4809601f7045
AhmedShahan <shahan.ahmed001@gmail.com>
2022-08-09 23:30:54 +0600

    Submit button added

--- NABU_PATIENT.py
+++ NABU_PATIENT.py
@@ -9,7 +9,8 @@ layout =[
 [Text("Date of Birth", size=(15,1)), Input( size=(30,3), background_color= "White")],
 [Text("Age", size=(15,1)), Input( size=(30,3), background_color= "White")],
 [Text("Email",size=(15,1)), Input( size=(30,3), background_color= "White")],
-[Text("Phone Number", size=(15,1)), Input( size=(30,3), background_color= "White")]
+[Text("Phone Number", size=(15,1)), Input( size=(30,3), background_color= "White")],
+[Text("\t"*3),Button("Submit")]
 
 ]
 
