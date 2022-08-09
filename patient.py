db1ef41a668fd7ce25725a644f7c1258f8e14001
NabilaRashid <nabila.rashid@northsouth.edu>
2022-08-09 20:37:38 +0600

    Paitent file is added

---
+++ patient.py
@@ -0,0 +1,42 @@
+from PySimpleGUI import *
+
+import view as view
+
+
+def patientwindow():
+    theme("PythonPlus")
+    theme_input_background_color("white")
+    theme_input_text_color("Black")
+    signup="signup.png"
+    signin="signin.png"
+    back="back.gif"
+
+    frame=[
+        [
+        ReadFormButton('', button_color="#001d3c",image_filename=signup, image_size=(300, 80), image_subsample=2, border_width=0,key="signup")],
+        [ReadFormButton('', button_color="#001d3c",image_filename=signin, image_size=(300, 80), image_subsample=2, border_width=0,key="signin"),
+        ],
+        [ReadFormButton('', button_color="#001d3c",image_filename=back, image_size=(300, 80), image_subsample=2, border_width=0,key="back")],
+    ]
+
+    layout=[
+        [Text("\t"),Image("eth3.png")],
+        [Text("***************************************************************"*10)],
+        [Image("patient.png"),
+        Frame("Sign as Patient",frame,font=("Jokerman",30),title_color="yellow")
+        
+        ], 
+        ]
+
+
+
+    doc=Window("Doctor Login",layout,size=(700,620),location=(500,100))
+
+
+    while True:
+        event,value=doc.read()
+        if event==WIN_CLOSED:
+            break
+        elif event=="back":
+            doc.close()
+            view.view()
