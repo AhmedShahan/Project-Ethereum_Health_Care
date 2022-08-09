66ab8a2974345b8a46139930f1b5e8f90237fad7
NabilaRashid <nabila.rashid@northsouth.edu>
2022-08-09 22:46:31 +0600

    Paitent form

--- NABU_PATIENT.py
+++ NABU_PATIENT.py
@@ -1,9 +1,16 @@
 from PySimpleGUI import * 
 layout =[ 
     
-
-
-
+[Text("Welcome to Paitent Form", font=('Arial', 16), size=(500,2), justification= ("center"))],
+[Text("Name", size=(15,1)), Input( size=(30,3), background_color= "White")],
+[Text("Present Address", size=(15,1)), Input( size=(30,3), background_color= "White")],
+[Text("Gender", size=(15,1)),Radio('Male', 'group_id'), Radio('Female', 'group_id')],
+[Text("Date of Birth", size=(15,1)), Input( size=(30,3), background_color= "White")],
+[Text("Age", size=(15,1)), Input( size=(30,3), background_color= "White")],
+[Text("Email",size=(15,1)), Input( size=(30,3), background_color= "White")],
+[Text("Phone Number", size=(15,1)), Input( size=(30,3), background_color= "White")]
 
 ]
 
+
+Window("Paitent Registration Form", layout, size=(700, 500) ).read()
